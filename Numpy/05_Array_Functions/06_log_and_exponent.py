b75319296008e7aef6c8a05e8fdd834d915ddd64
Shivsai Jagadale <shivsai1396@gmail.com>
2026-09-02 23:22:40 +0530

    Add files via upload

---
+++ Numpy/05_Array_Functions/06_log_and_exponent.py
@@ -0,0 +1,8 @@
+import numpy as np
+a1=np.random.random((3,3))
+a2=np.arange(9).reshape(3,3)
+a3=np.arange(8).reshape(4,2)
+print("Log of array a1 is:\n")
+print(np.log(a1))
+print("Exponent of array a2 is:\n")
+print(np.exp(a2))
