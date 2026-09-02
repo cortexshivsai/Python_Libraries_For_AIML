d5d362d7c100f484e123bdc1879e16d5b056f4ae
Shivsai Jagadale <shivsai1396@gmail.com>
2026-09-02 23:23:00 +0530

    Add files via upload

---
+++ Numpy/05_Array_Functions/07_round_floor_ceil.py
@@ -0,0 +1,8 @@
+import numpy as np
+a1=np.random.random((2,3))
+print("Round function gives:\n")
+print(np.round(a1*100))
+print("Floor function gives:\n")
+print(np.floor(a1*100))
+print("Ceil function gives:\n")
+print(np.ceil(a1*100))
