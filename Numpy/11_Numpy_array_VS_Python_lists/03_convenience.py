c1ce2026fd6cf903013bc844a1ae83bb24801dc5
Shivsai Jagadale <shivsai1396@gmail.com>
2026-09-07 22:05:44 +0530

    Add files via upload

---
+++ Numpy/11_Numpy_array_VS_Python_lists/03_convenience.py
@@ -0,0 +1,10 @@
+#Lists
+a=[1,2,3]
+b=[4,5,6]
+print("Lists join two list: ",a+b)
+
+#Numpy
+import numpy as np
+a=np.array([1,2,3])
+b=np.array([4,5,6])
+print("Numpy array adds corresponding elements: ",a+b)
