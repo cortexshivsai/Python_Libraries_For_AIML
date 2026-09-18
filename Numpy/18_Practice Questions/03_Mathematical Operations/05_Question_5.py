776232c0730e200b25b358034afe7123e2415e90
Shivsai Jagadale <shivsai1396@gmail.com>
2026-09-18 22:47:36 +0530

    Add files via upload

---
+++ Numpy/18_Practice Questions/03_Mathematical Operations/05_Question_5.py
@@ -0,0 +1,6 @@
+#Find the square of every element.
+import numpy as np
+a = np.array([10, 20, 30, 40, 50])
+b = np.array([1, 2, 3, 4, 5])
+print("Square of array a: ",np.square(a))
+print("Squares of array b: ",np.square(b))
