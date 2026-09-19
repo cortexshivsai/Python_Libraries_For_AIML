e8afcb3f2ccb32a82ebd41dd741a53ad6ccd373a
Shivsai Jagadale <shivsai1396@gmail.com>
2026-09-19 16:02:40 +0530

    Add files via upload

---
+++ Numpy/18_Practice Questions/03_Mathematical Operations/07_Question_7.py
@@ -0,0 +1,6 @@
+#Find the maximum.
+import numpy as np
+a = np.array([10, 20, 30, 40, 50])
+b = np.array([1, 2, 3, 4, 5])
+print("Maximum element of array a: ",np.max(a))
+print("Maximum element of array b: ",np.max(b))
