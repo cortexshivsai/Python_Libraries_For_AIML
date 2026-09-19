fb721787ed2befaa2a37abee52b1d90ac3ed6784
Shivsai Jagadale <shivsai1396@gmail.com>
2026-09-19 16:02:05 +0530

    Add files via upload

---
+++ Numpy/18_Practice Questions/03_Mathematical Operations/06_Question_6.py
@@ -0,0 +1,6 @@
+#Find the square root of every element.
+import numpy as np
+a = np.array([10, 20, 30, 40, 50])
+b = np.array([1, 2, 3, 4, 5])
+print("Squar Roots of every elemet of array a: ",np.sqrt(a))
+print("Squares Roots of every element of array b: ",np.sqrt(b))
