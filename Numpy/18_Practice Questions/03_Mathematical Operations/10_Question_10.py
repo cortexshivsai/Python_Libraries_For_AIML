b0dc13b7e951055d1e6fb0c30fa8d7ac1741b390
Shivsai Jagadale <shivsai1396@gmail.com>
2026-09-19 23:19:04 +0530

    Add files via upload

---
+++ Numpy/18_Practice Questions/03_Mathematical Operations/10_Question_10.py
@@ -0,0 +1,6 @@
+#Find the maedian.
+import numpy as np
+a = np.array([10, 20, 30, 40, 50])
+b = np.array([1, 2, 3, 4, 5])
+print("Median of array a: ",np.median(a))
+print("Median of array b: ",np.median(b))
