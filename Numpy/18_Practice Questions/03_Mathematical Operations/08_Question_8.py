955f013c814ef5b7a46abb99fc20cfcff88e1e54
Shivsai Jagadale <shivsai1396@gmail.com>
2026-09-19 23:17:59 +0530

    Add files via upload

---
+++ Numpy/18_Practice Questions/03_Mathematical Operations/08_Question_8.py
@@ -0,0 +1,6 @@
+#Find the minimum.
+import numpy as np
+a = np.array([10, 20, 30, 40, 50])
+b = np.array([1, 2, 3, 4, 5])
+print("minimum element of array a: ",np.min(a))
+print("minimum element of array b: ",np.min(b))
