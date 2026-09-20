50b4011620e8752b6eaaaa872cdece081ca3e8a1
Shivsai Jagadale <shivsai1396@gmail.com>
2026-09-20 23:50:18 +0530

    Add files via upload

---
+++ Numpy/18_Practice Questions/03_Mathematical Operations/11_Question_11.py
@@ -0,0 +1,6 @@
+#Find the Standard Deviation.
+import numpy as np
+a = np.array([10, 20, 30, 40, 50])
+b = np.array([1, 2, 3, 4, 5])
+print("Standar Deviation of array a: ",np.std(a))
+print("Standar Deviation of array b: ",np.std(b))
