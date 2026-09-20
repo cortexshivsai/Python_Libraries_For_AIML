b50edb8aa6be7f8fdddca7056754c7acef4407f9
Shivsai Jagadale <shivsai1396@gmail.com>
2026-09-20 23:53:19 +0530

    Add files via upload

---
+++ Numpy/18_Practice Questions/03_Mathematical Operations/12_Question_12.py
@@ -0,0 +1,6 @@
+#Find the variance.
+import numpy as np
+a = np.array([10, 20, 30, 40, 50])
+b = np.array([1, 2, 3, 4, 5])
+print("Variance of array a: ",np.var(a))
+print("Variance of array b: ",np.var(b))
