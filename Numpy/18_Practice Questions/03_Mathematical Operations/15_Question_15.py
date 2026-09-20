3c4aae154d5ac811fcaf9b522d4c05bda6a34782
Shivsai Jagadale <shivsai1396@gmail.com>
2026-09-20 23:55:47 +0530

    Add files via upload

---
+++ Numpy/18_Practice Questions/03_Mathematical Operations/15_Question_15.py
@@ -0,0 +1,6 @@
+#Find the Cumulative product.
+import numpy as np
+a = np.array([10, 20, 30, 40, 50])
+b = np.array([1, 2, 3, 4, 5])
+print("Cumulaive product of array a: ",np.cumprod(a))
+print("Cumulaive product of array b: ",np.cumprod(b))
