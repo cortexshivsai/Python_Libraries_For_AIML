765f72621afa3e65a4d0adc99f5b1b8974a07e04
Shivsai Jagadale <shivsai1396@gmail.com>
2026-09-20 23:54:33 +0530

    Add files via upload

---
+++ Numpy/18_Practice Questions/03_Mathematical Operations/14_Question_14.py
@@ -0,0 +1,6 @@
+#Find the Cumulative sum.
+import numpy as np
+a = np.array([10, 20, 30, 40, 50])
+b = np.array([1, 2, 3, 4, 5])
+print("Cumulaive sum of array a: ",np.cumsum(a))
+print("Cumulaive sum of array b: ",np.cumsum(b))
